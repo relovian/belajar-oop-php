fa42d670a89586672b311ff572b45739874e01f7
Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
2026-01-04 11:53:33 +0700

    membuat property untuk class product dan menambahkan method Cetak Label di class product setelah itu menampilkan return dari method Cetak Label

--- product.php
+++ product.php
@@ -1,11 +1,31 @@
 <?php 
 
 class Product {
+    public $judul = "judul",
+           $pengarang = "pengarang",
+           $penerbit = "penerbit",
+           $stok = 0;
 
+    public function CetakLabel($judul, $pengarang){
+        return "Judul Komik : $judul, Pengarang : $pengarang";
+    }
+        
 }
 
 $product1 = new Product();
-$product2 = new Product();
-$product3 = new Product();
+$product1->judul = "Doraemon";
+$product1->pengarang = "Fujiko F.Fujio";
+$product1->penerbit = "Elex Media Komputindo";
+$product1->stok = 100;
+
+echo $product1->CetakLabel($product1->judul, $product1->pengarang);
+
+echo "<br>";
+
+var_dump($product1);
+
+
+// $product2 = new Product();
+// $product3 = new Product();
 
 ?>
