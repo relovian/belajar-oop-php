fde02f9897fd88a35b6d3e9a31b454608a4a6930
Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
2026-01-05 20:36:58 +0700

    mempelajari inheritance problem sebelum masuk ke inheritance

--- product.php
+++ product.php
@@ -1,22 +1,43 @@
 <?php 
 
 class Product {
-    public $judul = "judul",
-           $pengarang = "pengarang",
-           $penerbit = "penerbit",
-           $stok = 0;
+    public $judul,
+           $pengarang,
+           $penerbit,
+           $jumlahHlm ,
+           $waktuMain,
+           $tipeProduct,
+           $stok;
 
     public function CetakLabel(){
-        return "Judul Komik : $this->judul, Pengarang : $this->pengarang";
+        return " judul : {$this->judul} <br> pengarang : {$this->pengarang}";
     }
 
-    public function __construct($judul, $pengarang, $penerbit, $stok){
+    public function __construct($judul  = "judul", $pengarang  = "pengarang", $penerbit  = "penerbit", $jumlahHlm = 0, $waktuMain  = 0, $tipeProduct = "tipe product", $stok = 0 ){
         $this->judul = $judul;
         $this->pengarang = $pengarang;
         $this->penerbit = $penerbit;
+        $this->jumlahHlm = $jumlahHlm;
+        $this->waktuMain = $waktuMain;
+        $this->tipeProduct = $tipeProduct;
         $this->stok = $stok;
     }
 
+    public function CetakInfoLengkap() {
+        $str = "- {$this->tipeProduct} <br> {$this->CetakLabel()} <br> penerbit :  {$this->penerbit} <br> stok :  {$this->stok}  ";
+
+        if($this->tipeProduct == "komik") {
+            $str .= " <br> jumlah halaman : {$this->jumlahHlm}  ";
+        } else {
+            $str .= " <br> waktu main : {$this->waktuMain} ";
+        }
+
+        return $str;
+    }
+
+    public function CetakInfoDetail() {
+        return "Komik : {$this->judul}, {$this->pengarang}, {$this->penerbit}, {$this->stok}, - {$this->jumlahHlm} Halaman ";
+    }
         
 }
 
@@ -28,7 +49,12 @@ class CetakInfoLengkap {
 
 }
 
-$product1 = new Product("Doraemon", "Fujiko F.Fujio", "Elex Media Kompution", 100);
+$product1 = new Product("Doraemon", "Fujiko F.Fujio", "Elex Media Kompution", 50, 0, "komik", 120);
+$product1 = new Product("Minecraft", "Mojang Studios", "Mojang Studios / Microsoft Studios", 0, 70, "game", 200);
+
+
+// echo $product1->CetakLabel();
+echo $product1->CetakInfoLengkap();
 
 // $product1->judul = "Doraemon";
 // $product1->pengarang = "Fujiko F.Fujio";
@@ -41,12 +67,18 @@ $product1 = new Product("Doraemon", "Fujiko F.Fujio", "Elex Media Kompution", 10
 // $product2->penerbit = "Shonen Jump";
 // $product2->stok = 50;
 
-echo $product1->CetakLabel();
-echo "<br>";
-// echo $product2->CetakLabel();
+// echo $product1->CetakLabel();
+// echo "<br>";
+// // echo $product2->CetakLabel();
 
-$CetakInfo = new CetakInfoLengkap();
-echo $CetakInfo->cetakInfo($product1);
+// $CetakInfo = new CetakInfoLengkap();
+// echo $CetakInfo->cetakInfo($product1);
+// echo "<br>";
+// echo "<br>";
+
+// echo $product1->CetakInfoDetail();
+
+
 
 
 // echo "<br>";
