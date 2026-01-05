d47200625968ac2c35a370209af3f5ae7b814563
Relovian Rahmadan S.P <relovianrahmadan154@gmail.com>
2026-01-05 19:18:12 +0700

    belajar construct dan mengubah cara manual mengisi sebuah objek menjadi otomatis atau simple

--- product.php
+++ product.php
@@ -6,23 +6,40 @@ class Product {
            $penerbit = "penerbit",
            $stok = 0;
 
-    public function CetakLabel($judul, $pengarang){
-        return "Judul Komik : $judul, Pengarang : $pengarang";
+    public function CetakLabel(){
+        return "Judul Komik : $this->judul, Pengarang : $this->pengarang";
     }
+
+    public function __construct($judul, $pengarang, $penerbit, $stok){
+        $this->judul = $judul;
+        $this->pengarang = $pengarang;
+        $this->penerbit = $penerbit;
+        $this->stok = $stok;
+    }
+
         
 }
 
-$product1 = new Product();
-$product1->judul = "Doraemon";
-$product1->pengarang = "Fujiko F.Fujio";
-$product1->penerbit = "Elex Media Komputindo";
-$product1->stok = 100;
+$product1 = new Product("Doraemon", "Fujiko F.Fujio", "Elec Media Komputindo", 100);
+$product2 = new Product("naruto", "masashi kishimoto", "shonen jump", 50);
+// $product1->judul = "Doraemon";
+// $product1->pengarang = "Fujiko F.Fujio";
+// $product1->penerbit = "Elex Media Komputindo";
+// $product1->stok = 100;
 
-echo $product1->CetakLabel($product1->judul, $product1->pengarang);
+// $product2 = new Product();
+// $product2->judul = "naruto";
+// $product2->pengarang = "masashi kishimoto";
+// $product2->penerbit = "Shonen Jump";
+// $product2->stok = 50;
 
+echo $product1->CetakLabel();
 echo "<br>";
+echo $product2->CetakLabel();
 
-var_dump($product1);
+// echo "<br>";
+
+// var_dump($product1);
 
 
 // $product2 = new Product();
